2dc8e48a906f6013e1ef7e0d254b845b278f7e94
ikrb01 <i_v_a_n_o_v@hotmail.com>
2020-07-20 21:51:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("pipeline in action: Hello, World!!!");
+    response.end("pipeline in action: Hello, World!!! Kubernetes + Azure + Docker");
 });
 
 const port = process.env.PORT || 1337;
